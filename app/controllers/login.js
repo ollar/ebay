8ccc496a670f3c7df8d6295ed866bd6199cddaf0
Oleg Larkin <oleg.larkin@nordigy.ru>
2018-03-27 18:48:33 +0300

    images dev

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -2,11 +2,20 @@ import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
 import { uuid } from 'ember-cli-uuid';
 
+import imageResize from '../utils/image-resize';
+
 export default Controller.extend({
     username: '',
     session: service(),
 
     actions: {
+        uploadImage(file) {
+            if (!file.type.match(/(png|jpg|jpeg)/ig)) return;
+
+            imageResize(file).then(image => {
+                console.log(image)
+            });
+        },
         submit() {
             if (!this.get('username')) return;
 
