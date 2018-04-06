e563d0f059b10135e6021547d27e55707082476e
Oleg Larkin <oleg.larkin@nordigy.ru>
2018-04-06 19:12:18 +0300

    small improvements 2

--- app/routes/application.js
+++ app/routes/application.js
@@ -6,7 +6,8 @@ import { hash } from 'rsvp';
 export default Route.extend(AuthenticatedRouteMixin, {
     model() {
         return hash({
-            products: this.get('store').findAll('product'),
+            products: this.get('store').findAll('product')
+                .then(products => products.filter(product => !product.get('sold'))),
             blocks: this.get('store').findAll('block'),
             bids: this.get('store').findAll('bid'),
         });
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,3 +1,13 @@
 import Route from '@ember/routing/route';
+import { hash } from 'rsvp';
 
-export default Route.extend({});
+export default Route.extend({
+    model() {
+        return hash({
+            products: this.get('store').findAll('product', {reload: true})
+                .then(products => products.filter(product => !product.get('sold'))),
+            blocks: this.get('store').findAll('block'),
+            bids: this.get('store').findAll('bid'),
+        });
+    }
+});
