2cf6a263c95019c4fa3229e0f9c00cfa13240c75
Oleg Larkin <oleg.larkin@nordigy.ru>
2018-04-06 19:15:16 +0300

    orders dev

--- app/routes/orders.js
+++ app/routes/orders.js
@@ -1,7 +1,12 @@
 import Route from '@ember/routing/route';
+import { computed } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default Route.extend({
+    session: service(),
+    me: computed.readOnly('session.data.authenticated.id'),
     model() {
-        return this.get('store').findAll('order');
+        return this.get('store').findAll('order')
+            .then(orders => orders.filter(order => order.get('buyer') === this.get('me')));
     }
 });
