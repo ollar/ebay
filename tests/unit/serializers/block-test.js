5ce74d550221e0b8c88aa57fd8abdf09d4641a00
Oleg Larkin <olegollar@gmail.com>
2018-04-08 23:05:24 +0300

    block seriaizer test

--- tests/unit/serializers/block-test.js
+++ tests/unit/serializers/block-test.js
@@ -21,4 +21,24 @@ module('Unit | Serializer | block', function(hooks) {
 
     assert.ok(serializedRecord);
   });
+
+  test('it serializes correctly', function(assert) {
+    let store = this.owner.lookup('service:store');
+    run(() => {
+      var record = store.createRecord('block', {
+        entity: 'product',
+        entry: {},
+      });
+
+      record.save();
+
+      let serializedRecord = record.serialize();
+
+      assert.equal(serializedRecord.index, 0);
+      assert.equal(serializedRecord.entity, 'product');
+      assert.equal(serializedRecord.previousHash, '');
+      assert.ok(serializedRecord.hash);
+      assert.ok(serializedRecord.timestamp);
+    });
+  });
 });
