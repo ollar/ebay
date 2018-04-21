6404a327624f12a163735879c27e585af20a8a95
Oleg Larkin <olegollar@gmail.com>
2018-04-21 09:28:22 +0300

    drawer dev 3

--- app/components/site-drawer.js
+++ app/components/site-drawer.js
@@ -1,12 +1,27 @@
 import Component from '@ember/component';
+import { inject as service } from '@ember/service';
+import { run, schedule } from '@ember/runloop';
 
 export default Component.extend({
     tagName: '',
     opened: false,
 
+    router: service('-routing'),
+
+    init() {
+        this._super(arguments);
+
+        const router = this.get('router');
+        router.addObserver('currentRouteName', this, 'closeDrawer');
+    },
+
+    closeDrawer() {
+        run(() => schedule('afterRender', () => this.set('opened', false)));
+    },
+
     actions: {
         toggleDrawer() {
-            this.toggleProperty('opened');
+            run(() => this.toggleProperty('opened'));
         },
     },
 });
